59374f391f29dbf80fc1006de1938c7fcfdd5bd6
+ <jeriannwest@yahoo.com>
2021-10-14 02:10:19 -0400

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ const { name, height, message } = require("../index.js");
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(name).toEqual("Joe");
     });
   });
 
